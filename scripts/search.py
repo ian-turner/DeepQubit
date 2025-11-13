7202bccbdac3daa9501eeff90f46fe5a1f41e189
ian-turner <i.r.turner55@gmail.com>
2025-11-13 15:22:05 -0500

    added config loading

--- scripts/search.py
+++ scripts/search.py
@@ -3,6 +3,7 @@ Script that uses A* search to synthesize a
 unitary matrix from an arbitrary gate set
 """
 import os
+import yaml
 from time import time
 from argparse import ArgumentParser
 import numpy as np
@@ -14,44 +15,70 @@ from utils.matrix_utils import load_matrix_from_file
 from utils.qasm_utils import *
 
 
+config = {
+    'max_steps': 1000,
+    'batch_size': 1000,
+    'epsilon': 0.01,
+    'nerf_dim': 0,
+    'path_weight': 0.2,
+    'encoding': 'matrix',
+    'gateset': 't,s,h,x,y,z',
+}
+
+
 if __name__ == '__main__':
     # parsing command line arguments
     parser = ArgumentParser()
     parser.add_argument('input', type=str)
+    parser.add_argument('-c', '--config', type=str)
     parser.add_argument('-n', '--nnet_dir', type=str, required=True)
     parser.add_argument('-o', '--output', type=str, required=True)
-    parser.add_argument('-m', '--max_steps', type=int, default=1e4)
-    parser.add_argument('-b', '--batch_size', type=int, default=1000)
-    parser.add_argument('-e', '--epsilon', type=float, default=1e-2)
+    parser.add_argument('-m', '--max_steps', type=int)
+    parser.add_argument('-b', '--batch_size', type=int)
+    parser.add_argument('-e', '--epsilon', type=float)
     parser.add_argument('-v', '--verbose', default=False, action='store_true')
-    parser.add_argument('-L', '--nerf_dim', type=int, default=0)
-    parser.add_argument('--path_weight', type=float, default=0.2)
-    parser.add_argument('--encoding', type=str, default='matrix',
+    parser.add_argument('-L', '--nerf_dim', type=int)
+    parser.add_argument('--path_weight', type=float)
+    parser.add_argument('--encoding', type=str,
                         choices=['matrix', 'hurwitz', 'quaternion', 'discrete'],
                         help='Encoding method of unitary matrix before passing to NNet')
-    parser.add_argument('--gateset', type=str, default='t,s,h,x,y,z')
-    args = parser.parse_args()
+    parser.add_argument('--gateset', type=str)
+    args = vars(parser.parse_args())
 
+    # overriding default config options from config file
+    if 'config' in args:
+        with open(args['config'], 'r') as f:
+            config_yaml = yaml.safe_load(f.read())
+            for x in config_yaml:
+                config[x] = config_yaml[x]
+        del args['config']
+
+    # overriding default config options with command line arguments
+    for x in args:
+        y = args[x]
+        if y != None:
+            config[x] = y
+ 
     start_time = time()
 
     # loading goal data
-    num_qubits, goal_matrix = load_matrix_from_file(args.input)
+    num_qubits, goal_matrix = load_matrix_from_file(args['input'])
 
     # environment setup
     env: QCircuit = QCircuit(
         num_qubits=num_qubits,
-        epsilon=args.epsilon,
-        encoding=args.encoding,
-        gateset=args.gateset,
-        L=args.nerf_dim,
+        epsilon=config['epsilon'],
+        encoding=config['encoding'],
+        gateset=config['gateset'],
+        L=config['nerf_dim'],
     )
     goal_states: List[QGoal] = [QGoal(goal_matrix)]
     start_states: List[QState] = [QState(tensor_product([I] * num_qubits))]
-    weights: List[float] = [args.path_weight]
+    weights: List[float] = [config['path_weight']]
 
     # loading heuristic function
     device, devices, on_gpu = nnet_utils.get_device()
-    nnet_weights_file: str = os.path.join(args.nnet_dir, 'target.pt')
+    nnet_weights_file: str = os.path.join(config['nnet_dir'], 'target.pt')
     heuristic_fn = nnet_utils.load_heuristic_fn(nnet_weights_file, device, \
                                                 on_gpu, env.get_v_nnet(), env)
 
@@ -64,8 +91,8 @@ if __name__ == '__main__':
 
     # running search
     step: int = 0
-    while np.any([not x.finished for x in astar.instances]) and step < args.max_steps:
-        astar.step(heuristic_fn, args.batch_size, verbose=args.verbose)
+    while np.any([not x.finished for x in astar.instances]) and step < config['max_steps']:
+        astar.step(heuristic_fn, config['batch_size'], verbose=args['verbose'])
         step += 1
     
     # getting path
@@ -82,7 +109,7 @@ if __name__ == '__main__':
         # converting circuit to OpenQASM 2.0
         qasm_str = path_to_qasm(path_actions, num_qubits)
         error = unitary_distance(qasm_to_matrix(qasm_str), goal_matrix)
-        with open(args.output, 'w') as f:
+        with open(args['output'], 'w') as f:
             f.write('// Gate-count: %d, T-count: %d, time: %.3fs, error: %.3e\n' % \
                     (gate_count, t_count, search_time, error))
             f.write(qasm_str)
@@ -91,4 +118,4 @@ if __name__ == '__main__':
               (gate_count, t_count, search_time))
 
     else:
-        print('Could not find circuit in %d steps' % args.max_steps)
+        print('Could not find circuit in %d steps' % config['max_steps'])
--- scripts/train.py
+++ scripts/train.py
@@ -1,47 +1,87 @@
+import os
 import torch
 from argparse import ArgumentParser
 from deepxube.training import avi
 from environments.qcircuit import QCircuit
+import yaml
+
+
+config = {
+    'num_qubits': 1,
+    'encoding': 'matrix',
+    'epsilon': 0.01,
+    'gateset': 't,s,h,x,y,z',
+    'step_max': 30,
+    'batch_size': 1000,
+    'itrs_per_update': 1000,
+    'max_itrs': 1e5,
+    'greedy_update_step_max': 100,
+    'num_update_procs': 10,
+    'perturb': False,
+    'nerf_dim': 0,
+}
 
 
 if __name__ == '__main__':
     # parsing command line arguments
     parser = ArgumentParser()
+    parser.add_argument('-c', '--config', type=str)
     parser.add_argument('--nnet_dir', type=str, required=True)
-    parser.add_argument('--num_qubits', type=int, required=True)
-    parser.add_argument('--encoding', type=str, default='matrix',
+    parser.add_argument('--num_qubits', type=int)
+    parser.add_argument('--encoding', type=str,
                         choices=['matrix', 'hurwitz', 'quaternion', 'discrete'],
                         help='Encoding method of unitary matrix before passing to NNet')
-    parser.add_argument('--epsilon', type=float, default=0.01,
+    parser.add_argument('--epsilon', type=float,
                         help='Tolerance value for solved condition')
-    parser.add_argument('--gateset', type=str, default='t,s,h,x,y,z')
-    parser.add_argument('--step_max', type=int, default=30)
-    parser.add_argument('--batch_size', type=int, default=100)
-    parser.add_argument('--itrs_per_update', type=int, default=1000)
-    parser.add_argument('--max_itrs', type=int, default=1e5)
-    parser.add_argument('--greedy_update_step_max', type=int, default=5)
-    parser.add_argument('--num_update_procs', type=int, default=5)
+    parser.add_argument('--gateset', type=str)
+    parser.add_argument('--step_max', type=int)
+    parser.add_argument('--batch_size', type=int)
+    parser.add_argument('--itrs_per_update', type=int)
+    parser.add_argument('--max_itrs', type=int)
+    parser.add_argument('--greedy_update_step_max', type=int)
+    parser.add_argument('--num_update_procs', type=int)
     parser.add_argument('--perturb', action='store_true')
-    parser.add_argument('-L', '--nerf_dim', type=int, default=0)
-    args = parser.parse_args()
-    
+    parser.add_argument('-L', '--nerf_dim', type=int)
+    args = vars(parser.parse_args())
+
+    # overriding default config options from config file
+    if 'config' in args:
+        with open(args['config'], 'r') as f:
+            config_yaml = yaml.safe_load(f.read())
+            for x in config_yaml:
+                config[x] = config_yaml[x]
+        del args['config']
+
+    # overriding default config options with command line arguments
+    for x in args:
+        y = args[x]
+        if y != None:
+            config[x] = y
+
     # environment setup
     env = QCircuit(
-        num_qubits=args.num_qubits,
-        epsilon=args.epsilon,
-        L=args.nerf_dim,
-        perturb=args.perturb,
-        encoding=args.encoding,
+        num_qubits=config['num_qubits'],
+        epsilon=config['epsilon'],
+        L=config['nerf_dim'],
+        perturb=config['perturb'],
+        encoding=config['encoding'],
     )
+
+    # copying config options
+    if not os.path.exists(config['nnet_dir']):
+        os.mkdir(config['nnet_dir'])
+    config_file_copy = os.path.join(config['nnet_dir'], 'config.yaml')
+    with open(config_file_copy, 'w') as f:
+        yaml.safe_dump(config, f)
 
     # running approximate value iteration
     avi.train(
         env=env,
-        nnet_dir=args.nnet_dir,
-        step_max=args.step_max,
-        batch_size=args.batch_size,
-        itrs_per_update=args.itrs_per_update,
-        max_itrs=args.max_itrs,
-        greedy_update_step_max=args.greedy_update_step_max,
-        num_update_procs=args.num_update_procs,
+        nnet_dir=config['nnet_dir'],
+        step_max=config['step_max'],
+        batch_size=config['batch_size'],
+        itrs_per_update=config['itrs_per_update'],
+        max_itrs=config['max_itrs'],
+        greedy_update_step_max=config['greedy_update_step_max'],
+        num_update_procs=config['num_update_procs'],
     )
