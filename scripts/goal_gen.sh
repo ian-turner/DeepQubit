b10d8883dd055997d79d71abfbfe60b6abf27b67
Ian Turner <i.r.turner55@gmail.com>
2026-01-19 00:23:47 -0500

    added solve script

--- scripts/goal_gen.sh
+++ scripts/goal_gen.sh
@@ -3,6 +3,6 @@
 
 deepxube problem_inst --domain qcircuit.n1_R \
                       --step_max 1 \
-                      --num 10000 \
+                      --num 1000 \
                       --file tmp/n1_goals_R.pkl \
                       --redo
---
+++ scripts/solve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+deepxube solve --domain qcircuit.n1_L15_P \
+               --heur_type QFix \
+               --pathfind bwqs.1_0.8_0.1 \
+               --file tmp/n1_goals_R.pkl \
+               --results tmp/n1_paths_R/ \
+               --redo
