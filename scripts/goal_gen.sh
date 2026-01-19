7247870c7f4a8a541421047463594901c24c23ac
Ian Turner <i.r.turner55@gmail.com>
2026-01-19 00:37:45 -0500

    updated scripts

--- scripts/goal_gen.sh
+++ scripts/goal_gen.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 deepxube problem_inst --domain qcircuit.n1_R \
                       --step_max 1 \
                       --num 1000 \
-                      --file tmp/n1_goals_R.pkl \
+                      --file tmp/n1_goals_R_1K.pkl \
                       --redo
--- scripts/solve.sh
+++ scripts/solve.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 deepxube solve --domain qcircuit.n1_L15_P \
                --heur_type QFix \
                --pathfind bwqs.1_0.8_0.1 \
-               --file tmp/n1_goals_R.pkl \
-               --results tmp/n1_paths_R/ \
+               --file tmp/n1_goals_R_1K.pkl \
+               --results tmp/n1_paths_R_1K/ \
                --redo
