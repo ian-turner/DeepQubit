564daf534464b00fe4cbaa3686355d4e64f66b54
Ian Turner <i.r.turner55@gmail.com>
2026-01-18 23:58:59 -0500

    updated train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,7 +4,7 @@ NNET_NAME="n1_L15_P"
 deepxube train --domain qcircuit.$NNET_NAME \
                --heur resnet_fc.1000H_2B_bn \
                --heur_type QFix \
-               --pathfind bwqs \
+               --pathfind bwqs.1_0.8_0.1 \
                --dir tmp/$NNET_NAME \
                --batch_size 10000 \
                --max_itrs 100000 \
