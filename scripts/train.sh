7c9fcd887713a4c5673a46399de549b87eaf786b
Ian Turner <i.r.turner55@gmail.com>
2026-01-19 23:49:44 -0500

    changed scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NNET_NAME="n1_L15_P"
+NNET_NAME="n1_L15_P_H"
 deepxube train --domain qcircuit.$NNET_NAME \
-               --heur resnet_fc.1000H_2B_bn \
+               --heur resnet_fc.2000H_2B_bn \
                --heur_type QFix \
                --pathfind bwqs.1_0.8_0.1 \
                --dir tmp/$NNET_NAME \
