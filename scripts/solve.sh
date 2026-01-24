902333ef96aad40a5910bf861f0aabd43221b8ef
Ian Turner <i.r.turner55@gmail.com>
2026-01-24 15:44:54 -0500

    updated scripts with variables

--- scripts/solve.sh
+++ scripts/solve.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-NNET_NAME="n1_L15_P_H"
-deepxube solve --domain qcircuit.$NNET_NAME \
-               --heur resnet_fc.2000H_2B_bn \
-               --heur_file tmp/$NNET_NAME/heur.pt \
+DOMAIN="n1_L15_P_H_e0.001"
+HEUR="resnet_fc.2000H_3B_bn"
+PATHFIND="bwqs.100_0.8_0.0"
+TIME_LIMIT=10
+
+
+mkdir -p tmp/paths/$DOMAIN
+
+deepxube solve --domain qcircuit.$DOMAIN \
+               --heur $HEUR \
+               --heur_file tmp/$DOMAIN/$HEUR/heur.pt \
                --heur_type QFix \
-               --pathfind bwqs.1_1.0_0.0 \
-               --file tmp/n1_goals_R_1K.pkl \
-               --results tmp/n1_paths_R_1K/ \
-               --time_limit 10 \
+               --pathfind $PATHFIND \
+               --file tmp/goals/n1_goals_R_1K.pkl \
+               --results tmp/paths/$DOMAIN/$HEUR \
+               --time_limit $TIME_LIMIT \
                --redo
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-NNET_NAME="n1_L15_P_H"
-deepxube train --domain qcircuit.$NNET_NAME \
-               --heur resnet_fc.2000H_2B_bn \
+DOMAIN="n1_L15_P_H_e0.001"
+HEUR="resnet_fc.2000H_3B_bn"
+PATHFIND="bwqs.1_0.8_0.0"
+
+BATCH_SIZE=20000
+MAX_ITRS=100000
+STEP_MAX=1200
+SEARCH_ITRS=1000
+TEST_SEARCH_ITRS=400
+PROCS=20
+
+
+mkdir -p tmp/heurs/$DOMAIN
+
+deepxube train --domain qcircuit.$DOMAIN \
+               --heur $HEUR \
                --heur_type QFix \
-               --pathfind bwqs.1_0.8_0.1 \
-               --dir tmp/$NNET_NAME \
-               --batch_size 10000 \
-               --max_itrs 100000 \
-               --procs 20 \
-               --step_max 1000 \
-               --search_itrs 1000 \
-               --t_file tmp/n1_goals_R_1K.pkl \
-               --t_pathfinds bwqs \
-               --t_search_itrs 100 \
+               --pathfind $PATHFIND \
+               --dir tmp/heurs/$DOMAIN/$HEUR \
+               --batch_size $BATCH_SIZE \
+               --max_itrs $MAX_ITRS \
+               --procs $PROCS \
+               --step_max $STEP_MAX \
+               --search_itrs $SEARCH_ITRS \
+               --t_file tmp/goals/n1_goals_R_1K.pkl \
+               --t_pathfinds $PATHFIND \
+               --t_search_itrs $TEST_SEARCH_ITRS \
                --up_v
