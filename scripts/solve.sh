ec47d715cbc8f4cce7f4e2a941e2796a91d649a9
Ian Turner <i.r.turner55@gmail.com>
2026-03-02 10:52:06 -0500

    updated scripts

--- scripts/solve.sh
+++ scripts/solve.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-DOMAIN="n1_L15_P_H_e0.001"
-HEUR="resnet_fc.2000H_3B_bn"
-PATHFIND="bwqs.100_0.8_0.0"
+DOMAIN="n1_L15_P_H_e0.01"
+HEUR="resnet_fc.2000H_2B_bn"
+PATHFIND="bwqs.1_1.0_0.0"
 TIME_LIMIT=10
 
 
-mkdir -p tmp/paths/$DOMAIN
+mkdir -p tmp/$DOMAIN/$HEUR/paths/$PATHFIND
 
 deepxube solve --domain qcircuit.$DOMAIN \
                --heur $HEUR \
                --heur_file tmp/$DOMAIN/$HEUR/heur.pt \
                --heur_type QFix \
                --pathfind $PATHFIND \
-               --file tmp/goals/n1_goals_R_1K.pkl \
-               --results tmp/paths/$DOMAIN/$HEUR \
+               --file tmp/n1_goals_R_1K.pkl \
+               --results tmp/$DOMAIN/$HEUR/paths/$PATHFIND \
                --time_limit $TIME_LIMIT \
                --redo
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 DOMAIN="n1_L15_P_H_e0.001"
-HEUR="resnet_fc.2000H_3B_bn"
-PATHFIND="bwqs.1_0.8_0.0"
+HEUR="resnet_fc.2000H_2B_bn"
+PATHFIND="bwqs.1_0.8_0.1"
 
 BATCH_SIZE=20000
 MAX_ITRS=100000
-STEP_MAX=1200
+STEP_MAX=1000
 SEARCH_ITRS=1000
-TEST_SEARCH_ITRS=400
-PROCS=20
+TEST_SEARCH_ITRS=100
+PROCS=24
 
 
-mkdir -p tmp/heurs/$DOMAIN
+mkdir -p tmp/$DOMAIN
 
 deepxube train --domain qcircuit.$DOMAIN \
                --heur $HEUR \
                --heur_type QFix \
                --pathfind $PATHFIND \
-               --dir tmp/heurs/$DOMAIN/$HEUR \
+               --dir tmp/$DOMAIN/$HEUR \
                --batch_size $BATCH_SIZE \
                --max_itrs $MAX_ITRS \
                --procs $PROCS \
                --step_max $STEP_MAX \
                --search_itrs $SEARCH_ITRS \
-               --t_file tmp/goals/n1_goals_R_1K.pkl \
+               --t_file tmp/n1_goals_R_1K.pkl \
                --t_pathfinds $PATHFIND \
                --t_search_itrs $TEST_SEARCH_ITRS \
                --up_v
